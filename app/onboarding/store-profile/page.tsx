00b4ccdbacc0481f8d0a07dfacc6fbcae1e682c5
Larry Custodio <custodioly@gmail.com>
2023-11-08 21:52:27 -0500

    feat: initialize repo

---
+++ app/onboarding/store-profile/page.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function StoreProfile() {
+  return <main>Store Store</main>;
+}
---
+++ app/onboarding/user-profile/page.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function UserProfile() {
+  return <main>User user</main>;
+}
